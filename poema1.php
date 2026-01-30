2fecf5b54199afb3077dfc47d5c447bc9b285260
RUBENDARIO2003 <rubendario2003iy@gmail.com>
2026-01-29 20:34:16 -0500

    poema1.php

---
+++ poema1.php
@@ -0,0 +1,18 @@
+<?php
+$titulo = "Poema 1 - pagina-web";
+include "header.php";
+?>
+
+<div class="poema">
+  <h1>Poema al Amanecer</h1>
+  <p>
+    El sol despierta lento en el cielo,<br>
+    pintando de oro la mañana fría,<br>
+    y en cada rayo deja un anhelo<br>
+    de comenzar de nuevo el día.
+  </p>
+</div>
+
+<footer>© 2026 - pagina-web</footer>
+</body>
+</html>
